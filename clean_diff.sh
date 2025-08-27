20b43d0562e0bfe8e0e56941d6e6d246cd38b9f2
Jeanne Allen <jeanne.allen@intimetec.com>
2025-08-27 09:46:22 -0600

    hoping this filter cleans up eol diffs

--- clean_diff.sh
+++ clean_diff.sh
@@ -4,9 +4,12 @@
 
 sed 's/ sap:VirtualizedContainerService\.HintSize="[0-9.]*,[0-9.]*"//g' "$1" | \
 sed '/<x:Boolean x:Key="IsExpanded">\(True\|False\)<\/x:Boolean>/d' | \
-sed '/<x:Boolean x:Key="IsPinned">\(True\|False\)<\/x:Boolean>/d'
+sed '/<x:Boolean x:Key="IsPinned">\(True\|False\)<\/x:Boolean>/d'  | \
+sed '/<av:Size x:Key="ShapeSize">[0-9.]*,[0-9.]*<\/av:Size>/d' | \
+sed $'s/\r$//' | \
+tr -d '\r'
 # Add this to git config: 
-# git config diff.nohintsize.textconv "$(pwd)/remove_hintsize.sh"
+# git config diff.cleandiff.textconv "$(pwd)/clean_diff.sh"
 
 # add to .gitattributes:
 #  diff=cleandiff
